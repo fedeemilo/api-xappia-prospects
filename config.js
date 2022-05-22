d7a8efd7c607afb49f2fdda9d2a8aa9230651ed7
Federico Milone <federico.milone@claro.com.ar>
2022-05-22 19:31:05 -0300

    update

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ process.env.PORT = process.env.PORT || 8000;
 // ============================
 //  Entorno
 // ============================
-console.log(process.env.NODE_ENV);
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 let baseUrl;
